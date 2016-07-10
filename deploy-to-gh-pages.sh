3ec7bb77b896de82ffbfcf509350030df06e48e6
june29 <june29.jp@gmail.com>
2016-07-10 17:09:24 +0900

    :wrench: Adjust deploy script

--- deploy-to-gh-pages.sh
+++ deploy-to-gh-pages.sh
@@ -3,37 +3,31 @@
 set -e
 pwd
 
-remote=$(git config remote.origin.url)
+remote="https://$GH_TOKEN@github.com/june29/irasutoya-data.git"
 
-siteSource="$1"
+dist="$1"
 
-if [ ! -d "$siteSource" ]
+if [ ! -d "$dist" ]
 then
     echo "Usage: $0 <site source dir>"
     exit 1
 fi
 
+rm -rf .git
+
 mkdir gh-pages-branch
 cd gh-pages-branch
 
 git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name "$GH_NAME" > /dev/null 2>&1
+git config --global user.name  "$GH_NAME" > /dev/null 2>&1
 git init
-git remote add --fetch origin "$remote"
 
-if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
-then
-    git checkout gh-pages
-    git rm -rf .
-else
-    git checkout --orphan gh-pages
-fi
+cp "../${dist}/irasutoya.json" .
 
-cp -a "../${siteSource}/." .
-
-git add -A
-git commit --allow-empty -m "Deploy by CircleCI"
-git push --force --quiet origin gh-pages > /dev/null 2>&1
+git add .
+git commit -m "Deploy by CircleCI"
+git remote add origin "$remote"
+git push --force --quiet origin master:gh-pages > /dev/null 2>&1
 
 cd ..
 rm -rf gh-pages-branch
